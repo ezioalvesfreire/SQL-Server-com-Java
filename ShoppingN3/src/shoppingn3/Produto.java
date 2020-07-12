e3ad66a8c1fb4f94513604bcfcfa848d953b9181
Ézio Alves <ezio.freire@catolicasc.org.br>
2020-07-12 17:51:46 -0300

    desenvolvendo a classe produto

--- ShoppingN3/src/shoppingn3/Produto.java
+++ ShoppingN3/src/shoppingn3/Produto.java
@@ -1,14 +1,54 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package shoppingn3;
 
-/**
- *
- * @author Ezio
- */
 public class Produto {
+    
+     int id_Produto;
+        String nome;
+	double preco;
+        int desconto; // adicionei
+
+    public Produto(int id_Produto, String nome, double preco, int desconto) {
+        this.id_Produto = id_Produto;
+        this.nome = nome;
+        this.preco = preco;
+        this.desconto = desconto;
+    }
+
+    public int getId_Produto() {
+        return id_Produto;
+    }
+
+    public void setId_Produto(int id_Produto) {
+        this.id_Produto = id_Produto;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    public int getDesconto() {
+        return desconto;
+    }
+
+    public void setDesconto(int desconto) {
+        this.desconto = desconto;
+    }
+     @Override
+	public String toString() {
+		return "Produto [id=" + id_Produto + ", nome=" + nome + ", preco=" + preco + "]";
+	}
+
     
 }
